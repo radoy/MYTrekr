9c92140b98bdd2f2c2f1e3f8907ec5b1ecc61a2c
radoy <rizal.adoy@gmail.com>
2021-05-18 09:29:50 +0700

    Initial Commit

---
+++ MYTrekr/MYTrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYTrekrApp.swift
+//  MYTrekr
+//
+//  Created by Muhammad Yusrizal on 18/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MYTrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
